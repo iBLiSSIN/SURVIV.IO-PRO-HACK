db0d747ef15f3179e1db6fe758be2dfe7c6a03c7
VNBPM on YT <77667370+iBLiSSIN@users.noreply.github.com>
2021-11-13 18:32:58 +0700

    Update CeilingsRemover.user.js

--- devs/CeilingsRemover.user.js
+++ devs/CeilingsRemover.user.js
@@ -3,7 +3,7 @@
 (function() {
     'use strict';
  
-var ceilings = "" 
+var ceilings = ""
 var func = {
     webpack_inject: (w, e, get) => {
         ceilings = get("03f4982a")
@@ -18,15 +18,17 @@ if(typeof window.webpackJsonp === 'function') {
         func,
         [["webpack_inject"]]
     ]);
-} 
+}
 Object.keys(ceilings).forEach(function(key) {
-    if(ceilings[key].ceiling) {
-    if(ceilings[key].type == "container_05" ) return
-
-      if(ceilings[key].ceiling.imgs) { 
-        Object.keys(ceilings[key].ceiling.imgs).forEach(function(ceilImg) {
-            ceilings[key].ceiling.imgs[ceilImg].sprite = ""
-      });
-    }
-  }
-});
+    if(ceilings[key].type === "building") {
+    if (ceilings[key].imgs.length > 1) {
+    if(ceilings[key].type == "container_05") return
+    ceilings[key].imgs.forEach(img => {
+     if (img.isCeiling) {
+            img.sprite= "https://raw.githubusercontent.com/iBLiSSIN/archive/main/lock/2CD6754F-AE61-4C37-BF0D-131C8841060A.png"
+         }
+      })
+   }
+}
+})
+})();
