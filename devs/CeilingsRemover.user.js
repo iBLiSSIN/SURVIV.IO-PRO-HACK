a3f04e5dd04c7204b1c38c7a2a98e522c8989069
VNBPM on YT <77667370+iBLiSSIN@users.noreply.github.com>
2021-11-13 18:52:21 +0700

    IceHacks type

--- devs/CeilingsRemover.user.js
+++ devs/CeilingsRemover.user.js
@@ -3,10 +3,10 @@
 (function() {
     'use strict';
  
-var ceilings = ""
+var roofs = ""
 var func = {
     webpack_inject: (w, e, get) => {
-        ceilings = get("03f4982a")
+        roofs = get("03f4982a")
     },
 };
  
@@ -19,11 +19,11 @@ if(typeof window.webpackJsonp === 'function') {
         [["webpack_inject"]]
     ]);
 }
-Object.keys(ceilings).forEach(function(key) {
-    if(ceilings[key].type === "building") {
-    if (ceilings[key].imgs.length > 1) {
-    if(ceilings[key].type == "container_05") return
-    ceilings[key].imgs.forEach(img => {
+Object.keys(roofs).forEach(function(obj) {
+    if(roofs[obj].type === "building") {
+    if (roofs[obj].imgs.length > 1) {
+    if(roofs[obj].type == "container_05") return
+    roofs[obj].imgs.forEach(img => {
      if (img.isCeiling) {
             img.sprite= "https://raw.githubusercontent.com/iBLiSSIN/archive/main/lock/2CD6754F-AE61-4C37-BF0D-131C8841060A.png"
          }
